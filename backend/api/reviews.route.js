7ee41ca3d8c01949cbd7e7c4353ad19281842db8
Amardeep Kumar <akumar26@univ.yo.oulu.fi>
2026-05-09 16:59:01 +0300

    first upload

---
+++ backend/api/reviews.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import ReviewCtrl from "./reviews.controller.js"
+
+const router =express.Router()
+
+router.route("/").get((req, res) => res.send("hello world"))
+
+export default router
